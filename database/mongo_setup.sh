accf03e17a7bddb529e9de1eedfc6de67d6f854f
Zhuqing <zengzhuqing@foxmail.com>
2015-03-28 18:57:24 +0800

    Add db path configuration in database

--- database/mongo_setup.sh
+++
@@ -1,6 +0,0 @@
-# Install MongoDB server
-sudo yum install python-pymongo.x86_64 mongodb.x86_64 mongodb-server.x86_64 -y
-# Start the service
-sudo /sbin/service mongod start
-# Start when boot
-sudo chkconfig mongod on
