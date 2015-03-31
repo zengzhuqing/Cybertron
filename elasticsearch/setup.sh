1b019b2dbca553bf15877572969b8ce8be3555d7
Zhuqing <zengzhuqing@foxmail.com>
2015-03-31 10:39:20 +0800

    Update elasticsearch to verson 1.5     Update setup.sh and README

--- elasticsearch/setup.sh
+++ elasticsearch/setup.sh
@@ -5,18 +5,28 @@ function error()
     exit 1
 }
 
+work_dir=/cores
+
 STEP=1
 echo "STEP:$STEP download elasticsearch package..."
-curl -L -O http://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.20.5.tar.gz
+wget -c https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.5.0.noarch.rpm
 echo "STEP:$STEP OK"
 
 STEP=`expr $STEP + 1`
 echo "STEP:$STEP extract elasticsearch package..."
-tar xmf *.tar.gz || error "tar error"
-mv elasticsearch-0.20.5 elasticsearch
+sudo rpm -iv elasticsearch-1.5.0.noarch.rpm
+sudo /sbin/chkconfig --add elasticsearch
+mkdir /cores/elasticsearch
+sudo chown elasticsearch.elasticsearch /cores/elasticsearch
+sudo sed -i 's/^#path.data: \/path\/to\/data$/path.data: \/cores\/elasticsearch/' /etc/elasticsearch/elasticsearch.yml 
+sudo /etc/init.d/elasticsearch start
 echo "STEP:$STEP OK"
 
 STEP=`expr $STEP + 1`
-echo "STEP:$STEP download elasticsearch python client..."
-curl -L -O https://pypi.python.org/packages/source/e/elasticsearch/elasticsearch-1.4.0.tar.gz
+echo "STEP:$STEP install elasticsearch python client package..."
+sudo yum install python-pip.noarch
+sudo pip install pbr
+sudo pip install elasticsearch 
+sudo pip install --upgrade urllib3
 echo "STEP:$STEP OK"
+
