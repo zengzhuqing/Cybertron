7e148146df716dce0a6e26af2f7dd8aa52020070
Zhuqing <zengzhuqing@foxmail.com>
2015-07-29 12:25:21 +0800

    Fix crossdomain

--- retrace_server/retrace-server-1.12/src/app.py
+++ retrace_server/retrace-server-1.12/src/app.py
@@ -6,11 +6,14 @@ from flask import session
 from flask import url_for
 from flask import redirect
 from flask import make_response
+from functools import update_wrapper
+from datetime import timedelta
 from retrace import *
 import re
 import fnmatch
 import urlparse
 import json
+from flask import make_response, request, current_app
 
 from retrace.Bugzilla_webservice import BugzillaServer
 
@@ -21,6 +24,49 @@ LONG_TYPES = { TASK_RETRACE: "Coredump retrace",
                TASK_VMCORE: "VMcore retrace",
                TASK_RETRACE_INTERACTIVE: "Coredump retrace - interactive",
                TASK_VMCORE_INTERACTIVE: "VMcore retrace - interactive" }
+
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, basestring):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, basestring):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            h['Access-Control-Allow-Credentials'] = 'true'
+            h['Access-Control-Allow-Headers'] = \
+                "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
 
 application= Flask(__name__)
 application.secret_key = 'abc'
@@ -136,6 +182,7 @@ def Login():
         return redirect(url_for("Index"))    
 
 @application.route('/Create_BG', methods=['POST'])
+@crossdomain(origin='*')
 def Create_BG():
     qs_base = []
     if "debug" in request.form and request.form["debug"] == "on":
